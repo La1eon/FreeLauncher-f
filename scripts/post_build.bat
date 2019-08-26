5c484a68f26502b10bdda52978a8aa8d2eb1fdd6
milosz 123456 <dommilosz@gmail.com>
2019-08-26 13:55:05 +0200

    Upload

--- scripts/post_build.bat
+++ scripts/post_build.bat
@@ -14,9 +14,8 @@ ECHO Step 1/4: Restoring original AssemblyInfo.cs...
 DEL /Q "%PROJECTDIR%\Properties\AssemblyInfo.cs"
 MOVE "%PROJECTDIR%\Properties\AssemblyInfo.cs.tmp" "%PROJECTDIR%\Properties\AssemblyInfo.cs"
 
-ECHO Step 2/4: Copying language and .exe files...
+ECHO Step 2/4: Copying language files...
 XCOPY /S /Y /F "%PROJECTDIR%\Translations\*" "%TARGETDIR%\MLauncher-langs\"
-XCOPY /S /Y /F "%TARGETDIR%\MLauncher.exe" "%PROJECTDIR%\"
 
 IF NOT "%CONFIGURATION%" == "Release" (
 	ECHO Detected non-release configuration.
